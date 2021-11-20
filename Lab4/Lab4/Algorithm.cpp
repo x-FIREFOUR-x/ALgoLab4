7acc7dc8de5cb8f093c5695e47d90b13fdf025bd
Alex <olexandrpasalsky@gmail.com>
2021-11-20 18:37:01 +0200

    algo work (mutation1 crossing_one_point & other help func)

--- Lab4/Lab4/Algorithm.cpp
+++ Lab4/Lab4/Algorithm.cpp
@@ -10,8 +10,29 @@ void Algorithm::genetic_algo(Graph& graph, int& count_iter)
 	}
 	else
 	{
+		int parent1 = 0;
+		int parent2 = rand() % (cliques.size() - 1) + 1;
+		pair<vector<int>,int> child = crossing_one_point(graph, parent1, parent2);
+
+		if (!child.first.empty())
+		{
+			int mut_will = rand() % 100;
+			if (mut_will < chance_mutation)
+			{
+				mutation1(graph, child);
+			}
+
+
+			if (sizes_cliques[sizes_cliques.size() - 1] < child.second)
+			{
+				cliques[cliques.size() - 1] = child.first;
+				sizes_cliques[sizes_cliques.size() - 1] = child.second;
+			}
+
+			sort_cliques();
+		}
+
 		count_iter++;
-		sort_cliques();
 	}
 	
 }
@@ -74,10 +95,145 @@ void Algorithm::sort_cliques()
 	sort(sizes_cliques.begin(), sizes_cliques.end(), [](int a, int b) { return a > b ; });
 }
 
+pair<vector<int>,int> Algorithm::crossing_one_point(Graph& graph, int pr1, int pr2)
+{
+	int n_lines = 1 + 1;     // одноточковий дає два відрізка 
+	int n_pr = 2;			//кількість батьків
+
+	vector<int> pr;			// індикси батьків
+	pr.push_back(pr1);
+	pr.push_back(pr2);
+
+	int point = rand() % (graph.get_size()+1);
+	
+
+	vector<int>points;						// точки поділу відрізка (begin, point, end) 
+	points.push_back(0);
+	points.push_back(point);
+	points.push_back(graph.get_size());
+
+	vector<vector<int>> childrens;			// нащадки
+	vector<int> sizes_childrens;
+
+	for (int i = 0; i < n_pr; i++)      // батько першого відрізка
+	{
+		for (int j = 0; j < n_pr; j++)			// батько другого выдрызка
+		{
+			if (i != j)
+			{
+					
+				vector<int> numb_parentr;						// номери батьків (0 або 1) в pr з яких буде братися k-ий відрізок
+				numb_parentr.push_back(i);
+				numb_parentr.push_back(j);
+
+				vector<int> child;
+				for (int k = 0; k < n_lines; k++)		// номер відрізка
+				{
+					int in_parent = pr[numb_parentr[k]];		// індекс батька
+					vector<int> line;				// вирізаєм k-ий відрізок в першого чи другого батька
+
+					auto it_l1_begin = line.begin();
+					auto it_pr_begin = cliques[in_parent].begin() + points[k];
+					auto it_pr_p = cliques[in_parent].begin() + points[k+1];
+					line.insert(it_l1_begin, it_pr_begin, it_pr_p);
+
+					child.insert(child.end(), line.begin(), line.end());	//вставляєм відрізок k-ий в сина
+				}
+
+				int amount_ver = calculate_size_clique(child);
+				if (is_clique(graph, child) && amount_ver > 0)		// перевіряєм чи син є клікою
+				{
+					childrens.push_back(child);
+					sizes_childrens.push_back(amount_ver);
+				}
+			}
+		}
+	}
+
+	if (sizes_childrens.size() > 0)
+	{
+		int index = search_best_child(graph, sizes_childrens);
+		pair<vector<int>, int> best_child(childrens[index], sizes_childrens[index]);
+		return best_child;
+	}
+	{
+		pair<vector<int>, int> best_child;
+		return best_child;
+	}
+	
+}
+
+bool Algorithm::mutation1(Graph& graph, pair<vector<int>, int>& child)
+{
+	bool success_mutation = true;
+
+	vector<int> mutat_child = child.first;
+	int size_mutat_child = child.second;
+
+	vector<int> index_0_genes;
+	for (int i = 0; i < mutat_child.size(); i++)
+	{
+		if (mutat_child[i] == 0)
+			index_0_genes.push_back(i);
+	}
+
+	if (!index_0_genes.empty())
+	{
+		int rand_0_gene = rand() % index_0_genes.size();
+		int in_gene = index_0_genes[rand_0_gene];
+
+		mutat_child[in_gene] = 1;
+		size_mutat_child++;
+
+		success_mutation = is_clique(graph, mutat_child);
+	}
+	else
+	{
+		success_mutation = false;
+	}
+	
+	if (success_mutation)
+	{
+		child.first = mutat_child;
+		child.second = size_mutat_child;
+	}
+
+	return success_mutation;
+}
+
+int Algorithm::calculate_size_clique(vector<int>& clq)
+{
+	int s = 0;
+	for (int i = 0; i < clq.size(); i++)
+	{
+		s += clq[i];
+	}
+	return s;
+}
+
+int Algorithm::search_best_child(Graph& graph,vector<int>& sizes_childrens)
+{
+	int min = graph.get_size() + 1;
+	int index;
+	for (int i = 0; i < sizes_childrens.size(); i++)
+	{
+		if (min > sizes_childrens[i])
+		{
+			min = sizes_childrens[i];
+			index = i;
+		}
+	}
+	return index;
+}
+
 int Algorithm::get_amount_cliques()
 {
 	return cliques.size();
 }
+
+
+
+
 
 vector<int> Algorithm::get_clique(int number)
 {
--- Lab4/Lab4/Algorithm.h
+++ Lab4/Lab4/Algorithm.h
@@ -6,6 +6,8 @@ using namespace std;
 class Algorithm
 {
 private:
+
+	int chance_mutation = 50;				// шанс мутації в процентах
 	vector<vector<int>> cliques;			// вектор клік в популяції (кліка булевий вектор з 1(є вершина) і 0(нема вершини)) 
 	vector<int> sizes_cliques;				// вектор розмірів клік (кількість 1 в кліках)
 public:
@@ -16,6 +18,12 @@ private:
 	bool is_clique(Graph& graph, vector<int> clq);
 	void sort_cliques();
 
+	pair<vector<int>, int> crossing_one_point(Graph& graph, int pr1, int pr2);
+
+	bool mutation1(Graph& graph, pair<vector<int>, int>& child);				// 1 різновид мутації (заміна випадкового 0 гена на 1 )
+
+	int calculate_size_clique(vector<int>& clq);								// підрахунок кількості вершин в кліці (clq)
+	int search_best_child(Graph& graph, vector<int>& sizes_childrens);			// пошук індикса найкращого сина по кількості їх вершин (sizes_childrens)
 public:
 	
 	int get_amount_cliques();						// отримати кількість клік (персон в популяції)
